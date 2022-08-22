2e41749c8fbc9ff318328a6efbce21c98b087e8b
Carlos Mejia <cmejia@equitel.com.co>
2022-08-21 21:55:48 -0500

    user schema

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: 'String',
+        required: true
+    },
+    password: {
+        type: 'String',
+        required: true
+    },
+    email: {
+        type: 'String',
+        required: true,
+        unique: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Provice a valid email address'
+        ]
+    },
+    displayName: {
+        type: 'String',
+        require: true
+    }
+}, {
+    timestamps: true,
+    versionKey: false
+});
+
+module.exports = model("User", userSchema);
