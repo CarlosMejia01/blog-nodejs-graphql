070525615c3ca5fe7c4a4095b0217cb2e14c7efa
Carlos Mejia <cmejia@equitel.com.co>
2022-08-21 23:28:37 -0500

    find user by id

--- graphql/queries.js
+++ graphql/queries.js
@@ -1,12 +1,30 @@
-const { GraphQLString, GraphQLList } = require('graphql');
+const { GraphQLList, GraphQLID } = require('graphql');
 const { UserType } = require('./types');
 const { User } = require('../models');
 
 const users = {
     type: new GraphQLList(UserType),
+    description: "Get all users",
     resolve() {
         return User.find();
     }
 };
 
-module.exports = { users };
+const user = {
+    type: UserType,
+    description: "Get user by id",
+    args: {
+        id: {
+            type: GraphQLID
+        }
+    },
+    resolve(_, args) {
+        return User.findById(args.id);
+    }
+};
+
+
+module.exports = {
+    users,
+    user
+};
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,12 +1,13 @@
 const { GraphQLSchema, GraphQLObjectType } = require("graphql");
-const { users } = require('./queries');
+const { users, user } = require('./queries');
 const { register, login } = require('./mutations');
 
 const QueryType = new GraphQLObjectType({
     name: 'QueryType',
     description: 'The root query type',
     fields: {
-        users
+        users,
+        user
     }
 });
 
