8ba89ae386312b414458ecc6660ea8588340e959
Carlos Mejia <cmejia@equitel.com.co>
2022-08-24 00:16:16 -0500

    find and delete a post (is only removed if it has the same token as the user

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -84,9 +84,31 @@ const updatePost = {
     }
 };
 
+const deletePost = {
+    type: GraphQLString,
+    description: "Delete a post",
+    args: {
+        postId: { type: GraphQLID }
+    },
+    resolve: async (_, { postId }, { verifiedUser }) => {
+        if (!verifiedUser) throw new Error("Unauthorized");
+
+        const postDeleted = await Post.findByIdAndDelete(
+            {
+                _id: postId,
+                authorId: verifiedUser._id
+            }
+        );
+        if (!postDeleted) throw new Error("Post not found");
+
+        return "Post deleted";
+    }
+}
+
 module.exports = {
     register,
     login,
     createPost,
-    updatePost
+    updatePost,
+    deletePost
 };
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,6 +1,6 @@
 const { GraphQLSchema, GraphQLObjectType } = require("graphql");
 const { users, user, posts, post } = require('./queries');
-const { register, login, createPost, updatePost } = require('./mutations');
+const { register, login, createPost, updatePost, deletePost } = require('./mutations');
 
 const QueryType = new GraphQLObjectType({
     name: 'QueryType',
@@ -20,7 +20,8 @@ const MutationType = new GraphQLObjectType({
         register,
         login,
         createPost,
-        updatePost
+        updatePost,
+        deletePost
     }
 })
 
